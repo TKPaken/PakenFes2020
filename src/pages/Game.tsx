68cf46a9b25d84b5b10edb55cf2225a22c3d1074
Akihisa Yagi <capra314cabra@gmail.com>
2020-10-27 00:43:18 +0900

    Add download urls

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -56,14 +56,14 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="Blow Shooting (nuhunune)"
                         picture="/assets/img/games/blow-shooting.png"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1cwAsU95wjdlELqJNKq-Z-2Q3cQMl-KcX?usp=sharing">
                         タイトルの通りターゲットに照準を合わせてSHOOTするゲームです。弾を撃つ時、ターゲットにヒットしたときなどに一々効果音をつけることで、爽快感のある仕上がりに出来たと思います。<br />
                         更にオリジナル要素として、Blow、つまり竜巻を撃つことが出来るようにしました(英語にはノーコメントでお願いします)。地形の後ろに隠れているターゲットを浮き上がらせてヒットする時の気持ちよさを、是非体験して頂きたいです！
                     </GameContents>
                     <GameContents
                         title="infinity run (Californium)"
                         picture="/assets/img/games/infinity-run.png"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1UIBk3xu760yAgUO2fMEY_oApZCsLnjkr?usp=sharing">
                         進んでいくとあるコインを取りながら、障害物を避けてできるだけ前に進む、というゲームです。<br />
                         スペースキーでジャンプし、障害物をよけましょう。押されてしまい、画面外に出てしまうとゲームオーバーです。<br />
                         コインは1つで777点加点されます。<br />
@@ -72,7 +72,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="Maze (Forested)"
                         picture="/assets/img/games/maze.png"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1aF4kdyBoh3e_IJ9YdtT-Dsx4FpUpBft-?usp=sharing">
                         これは迷路を探検するゲームにしたかったものです。ゲームと呼べるのかはわかりません。<br />
                         左右キーで方向転換ができます。スペースキーで前に進むことができます。<br />
                         ところどころに黄色い床があると思います。これを踏むと、赤い壁が消え、代わりに青い壁が出現します。もう一度踏むと元に戻ります。緑の床を探し、踏むことができたらクリアです。<br />
@@ -81,7 +81,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="ピコピコハンマー (penguinman)"
                         picture="/assets/img/games/pikohammer.png"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1S42owKPqEzkEJr3VgcXwLkSPkI6qAqcc?usp=sharing">
                         ピコピコハンマーから逃げるゲームです。<br />
                         ルールや操作方法はプログラム内に書いてあるので、そちらを見てください。<br />
                         一応裏実装として z ボタンを押すと何かが起きます。飽きてきたら押してみてください。
@@ -89,7 +89,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="Doppelganger (Thistle)"
                         picture="/assets/img/games/doppel.PNG"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1MjeGCI1vlhui63CJJ27cXs0vEdSV70C0?usp=sharing">
                         世界に彷徨うドッペルゲンガーに見つからないよう逃げるゲームです。<br />
                         ゴール地点の黄金の良く分からない像に触れることで脱出できます。<br />
                         身体を一切動かさずに移動し、たまにフィギュアスケーターのように回転するドッペルゲンガーの恐怖からあなたは逃げ切れるか？？
@@ -97,7 +97,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="ブロック崩し (cpcznksutbeoa)"
                         picture="/assets/img/games/block.jpg"
-                        downloadUrl=""
+                        downloadUrl="https://drive.google.com/drive/folders/1XjXkWC5dksO2-tNCixmtj4b8xIl6dLDv?usp=sharing"
                     >
                         このアプリは、パドルでボールを跳ね返してブロックを壊す、ブロック崩しです。<br />
                         パソコンのマウス等によって動く、矢印の横方向の位置にパドルの中心がきます。ボールを跳ね返して、できるだけ多くのブロックを壊しましょう！<br />
@@ -107,16 +107,16 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="Convex Hull Visualizer (Ebishu)"
                         picture="/assets/img/games/convex.JPG"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1ovq0KcJvWVb287ECLxu0yoxQbBcHNl81?usp=sharing">
                         凸包を計算するアルゴリズムの一つである「andrew's monotone chain algorithm」のビジュアライザです。<br />
                         andrew's monotone chain algorithmとは
                         点の数をnとすると、凸包をO(n log n)で求めることができるアルゴリズムです。<br />
                         点をx座標でソートした後、上側の凸包を下側の凸包を別々に計算し、併合して凸包を求めます。
                     </GameContents>
                     <GameContents
-                        title="FPS (primenumberzz)"
+                        title="TK_FPS (primenumberzz)"
                         picture="/assets/img/games/fps.png"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1luyv-6TIlNRPoV54zh_9K6aZwSiC-iUo?usp=sharing">
                         Unity公式のFPS Microgame Templateを利用し、チュートリアルを見ながら作りました。<br />
                         FPS Microgame Templateを使うとプログラミングの知識やコーディングも必要なく、
                         初心者の僕でもUnityの使い方を学ぶことができました。<br />
@@ -127,7 +127,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="CrammingGame2.1.1 (Thistle)"
                         picture="/assets/img/games/cramming.PNG"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1Q6ySLBOoRbi4OqewHy9Lz093puJn5JQI?usp=sharing">
                         期末対策用に作ったアプリケーションです。<br />
                         簡単に説明すると「デジタル無限単語帳 With 問題セット機能付き」です。<br />
                         自分で作った問題集から無限に問題を出してくれます。<br />
@@ -136,7 +136,7 @@ class GameComponent extends React.Component<IGameComponentProps, IGameComponentS
                     <GameContents
                         title="fps game (sandyoji)"
                         picture="/assets/img/games/fps-game.PNG"
-                        downloadUrl="">
+                        downloadUrl="https://drive.google.com/drive/folders/1r65BvJb-1FyEHJEnUmX1gfvGwp0SMrGa?usp=sharing">
                         これはユニティ公式が作成したfpsのチュートリアルでできるゲームを少し改造したものです。
                     </GameContents>
                 </div>
